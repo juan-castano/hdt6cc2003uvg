32948aefd9180b86b9f3e4f7dfcc3f8df2166ca6
dyhl22 <dyhl22@2d485c6e-a922-11de-ac05-951101a8bf3e>
2009-09-30 06:10:08 +0000

    Por hacer: Clase Dato para manejar mejor los elementos genericos Implementar la interfaz Comparable a la clase Dato Clase principal
    Por terminar:
    Arbol 2-3

--- trunk/Aboles23/src/Arbol23.java
+++ trunk/Aboles23/src/Arbol23.java
@@ -1,16 +1,93 @@
 
 public class Arbol23<E> {
-    Nodo raiz;
+    private Nodo raiz = new Nodo();
+
 
     public int buscar(E dato) {
-        return 0;
+        int hijo;
+        Nodo actual = raiz;
+        while(true) {
+            if((hijo = actual.buscar(dato)) != -1) 
+                return hijo;
+            else if(actual.esHoja())
+                return -1;
+            else
+                actual = sigHijo(actual, dato);
+        }
+    
+
+     
     }
 
     public void insertar(E dato) {
-        
+        Nodo actual = raiz;
+        E datoTemp = dato;
+
+        while(true) {
+            if(actual.lleno()) {
+                split(actual);
+                actual = actual.getPadre();
+                actual = sigHijo(actual, dato);
+                
+            }
+            else if(actual.esHoja())
+                break;
+            else
+                actual = sigHijo(actual, dato);
+        }
+
+        actual.insertar(datoTemp);
     }
 
     public void partir(Nodo nodo) {
+        E dato2, dato3;
+        Nodo padre, hijo2, hijo3;
+        int index;
+
+        dato3 = (E) nodo.remover();
+        dato2 = (E) nodo.remover();
+        hijo2 = nodo.quitarHijo(2);
+        hijo3 = nodo.quitarHijo(3);
+        Nodo nuevo = new Nodo();
+
+        if(nodo == raiz) {
+            raiz = new Nodo();
+            padre = raiz;
+            raiz.nuevoHijo(0, nodo);
+
+        }
+        else
+            padre = nodo.getPadre();
+
+        index = padre.insertar(dato2);
+
+        int n = padre.getCantidad();
+
+        for(int i = n; i > index; i--) {
+            Nodo temp = padre.quitarHijo(i);
+            padre.nuevoHijo(i+1, temp);
+        }
+
+        padre.nuevoHijo(index+1, nuevo);
+
+        nuevo.insertar(dato3);
+        nuevo.nuevoHijo(0, hijo2);
+        nuevo.nuevoHijo(1, hijo3);
+    }
+
+    public Nodo sigHijo(Nodo nodo, E dato) {
+        int i;
+        
+        int cantidad = nodo.getCantidad();
+        for(i = 0; i < cantidad; i++) {
+            if(dato.compareTo)
+        }
+
+        return nodo.getHijo(i);
+    }
+
+    public void despliegar() {
         
     }
 }
+s
